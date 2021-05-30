e1b20715a12b3135e1f71af7dabec738b27d0c2e
Denfit9 <d03062002@gmail.com>
2021-05-30 17:51:58 +0300

    f

---
+++ NeSdannoe/CSharpLab7/CSharpLab7/Fraction.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace СSharpLab7
+{
+    class Fraction : IComparer<Fraction>
+    {
+        public Fraction()
+        {
+
+        }
+
+        public Fraction(int numerator, int denomerator)
+        {
+            this.numerator = numerator;
+            this.denomerator = denomerator;
+        }
+
+        public double ToDouble()
+        {
+            double doubleFraction = (double)numerator / (double)denomerator;
+            return doubleFraction;
+        }
+
+        public int ToInt()
+        {
+            double doubleFraction = (double)numerator / (double)denomerator;
+            Console.WriteLine(doubleFraction);
+            int intFraction = (int)Math.Round(doubleFraction, MidpointRounding.AwayFromZero);
+            return intFraction;
+        }
+
+        public override string ToString()
+        {
+            int numeratorLength = numerator.ToString().Length;
+            int denomeratorLength = denomerator.ToString().Length;
+            int hyphenLength = (numeratorLength > denomeratorLength) ? numeratorLength : denomeratorLength;
+            string hyphen = "";
+            for (int i = 0; i < hyphenLength; i++)
+            {
+                hyphen += "-";
+            }
+            return numerator.ToString() + "\n" + hyphen + "\n" + denomerator.ToString() + "\n";
+        }
+
+        public string ToSpecialString()
+        {
+            string numeratorSpecialString = "";
+            numeratorSpecialString = "I think your number is " + numerator + "out of " + denomerator;
+            return numeratorSpecialString;
+        }
+        public static explicit operator double(Fraction fraction)
+        {
+            double doubleFraction = (double)fraction.numerator / (double)fraction.denomerator;
+            return doubleFraction;
+        }
+
+        public static explicit operator int(Fraction fraction)
+        {
+            double doubleFraction = (double)fraction.numerator / (double)fraction.denomerator;
+            Console.WriteLine(doubleFraction);
+            int intFraction = (int)Math.Round(doubleFraction, MidpointRounding.AwayFromZero);
+            return intFraction;
+        }
+
+        public static Fraction operator +(Fraction firstArg, Fraction secondArg)
+        {
+            if (firstArg.denomerator == secondArg.denomerator)
+            {
+                Fraction fraction = new Fraction(firstArg.numerator + secondArg.numerator, firstArg.denomerator);
+                Reduction(ref fraction);
+                return fraction;
+            }
+            else
+            {
+                int temp = firstArg.denomerator;
+                firstArg.denomerator *= secondArg.denomerator;
+                firstArg.numerator *= secondArg.denomerator;
+                secondArg.denomerator *= temp;
+                secondArg.numerator *= temp;
+                Fraction fraction = new Fraction(firstArg.numerator + secondArg.numerator, firstArg.denomerator);
+                Reduction(ref fraction);
+                return fraction;
+            }
+
+        }
+        public static Fraction operator -(Fraction firstArg, Fraction secondArg)
+        {
+            if (firstArg.denomerator == secondArg.denomerator)
+            {
+                return new Fraction(firstArg.numerator - secondArg.numerator, firstArg.denomerator);
+            }
+            else
+            {
+                int temp = firstArg.denomerator;
+                firstArg.denomerator *= secondArg.denomerator;
+                firstArg.numerator *= secondArg.denomerator;
+                secondArg.denomerator *= temp;
+                secondArg.numerator *= temp;
+                Fraction fraction = new Fraction(firstArg.numerator - secondArg.numerator, firstArg.denomerator);
+
+                return fraction;
+            }
+        }
+        public static Fraction operator *(Fraction firstArg, Fraction secondArg)
+        {
+            return new Fraction(firstArg.numerator * secondArg.numerator, firstArg.denomerator * secondArg.denomerator);
+        }
+        public static Fraction operator /(Fraction firstArg, Fraction secondArg)
+        {
+            return new Fraction(firstArg.numerator * secondArg.denomerator, firstArg.denomerator * secondArg.numerator);
+        }
+
+        public static bool operator >(Fraction firstArg, Fraction secondArg)
+        {
+            Reduction(ref firstArg);
+            Reduction(ref secondArg);
+            Fraction comparer = new Fraction();
+            int result = comparer.Compare(firstArg, secondArg);
+            if (result == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(Fraction firstArg, Fraction secondArg)
+        {
+            Reduction(ref firstArg);
+            Reduction(ref secondArg);
+            Fraction comparer = new Fraction();
+            int result = comparer.Compare(firstArg, secondArg);
+            if (result == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator ==(Fraction firstArg, Fraction secondArg)
+        {
+            Reduction(ref firstArg);
+            Reduction(ref secondArg);
+            Fraction comparer = new Fraction();
+            int result = comparer.Compare(firstArg, secondArg);
+            if (result == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Fraction firstArg, Fraction secondArg)
+        {
+            Reduction(ref firstArg);
+            Reduction(ref secondArg);
+            Fraction comparer = new Fraction();
+            int result = comparer.Compare(firstArg, secondArg);
+            if (result != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static int CommonDenominator(Fraction fraction)
+        {
+            int numerator = Math.Abs(fraction.numerator);
+            int denomerator = Math.Abs(fraction.denomerator);
+            while (denomerator != 0 && numerator != 0)
+            {
+                if (numerator % denomerator > 0)
+                {
+                    int temp = numerator;
+                    numerator = denomerator;
+                    denomerator = temp % denomerator;
+                }
+                else
+                {
+                    return denomerator;
+                }
+            }
+            if (denomerator != 0 && numerator != 0)
+            {
+                return denomerator;
+            }
+            return denomerator;
+        }
+
+        public static void Reduction(ref Fraction fraction)
+        {
+            int cd = CommonDenominator(fraction);
+            fraction.numerator /= cd;
+            fraction.denomerator /= cd;
+        }
+
+        public void SetNumerator(int value)
+        {
+            numerator = value;
+        }
+        public void SetDenomerator(int value)
+        {
+            denomerator = value;
+        }
+        public int GetNumerator()
+        {
+            return numerator;
+        }
+        public int GetDenomerator()
+        {
+            return denomerator;
+        }
+        public int Compare(Fraction firstArg, Fraction secondArg)
+        {
+            if (firstArg.GetDenomerator() == secondArg.GetDenomerator())
+            {
+                if (firstArg.GetNumerator() > secondArg.GetNumerator())
+                {
+                    return 1;
+                }
+                else if (firstArg.GetNumerator() == secondArg.GetNumerator())
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                int temp = firstArg.GetDenomerator();
+                firstArg.SetDenomerator(firstArg.GetDenomerator() * secondArg.GetDenomerator());
+                firstArg.SetNumerator(firstArg.GetNumerator() * secondArg.GetDenomerator());
+                secondArg.SetDenomerator(secondArg.GetDenomerator() * temp);
+                secondArg.SetNumerator(secondArg.GetNumerator() * temp);
+                if (firstArg.GetNumerator() > secondArg.GetNumerator())
+                {
+                    return 1;
+                }
+                else if (firstArg.GetNumerator() == secondArg.GetNumerator())
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+        public static void FractionSum(Fraction firstArg, Fraction secondArg, int variantOfInput)
+        {
+            if (variantOfInput == 1)
+            {
+                Console.WriteLine(firstArg + secondArg);
+            }
+            else
+            {
+                Console.WriteLine((firstArg + secondArg).ToSpecialString());
+            }
+
+        }
+
+
+        public static void FractionDiff(Fraction firstArg, Fraction secondArg, int variantOfInput)
+        {
+            if (variantOfInput == 1)
+            {
+                Console.WriteLine(firstArg - secondArg);
+            }
+            else
+            {
+                Console.WriteLine((firstArg - secondArg).ToSpecialString());
+            }
+
+        }
+
+
+        public static void FractionMult(Fraction firstArg, Fraction secondArg, int variantOfInput)
+        {
+            if (variantOfInput == 1)
+            {
+                Console.WriteLine(firstArg * secondArg);
+            }
+            else
+            {
+                Console.WriteLine((firstArg * secondArg).ToSpecialString());
+            }
+        }
+        public static void FractionDiv(Fraction firstArg, Fraction secondArg, int variantOfInput)
+        {
+            if (variantOfInput == 1)
+            {
+                Console.WriteLine(firstArg / secondArg);
+            }
+            else
+            {
+                Console.WriteLine((firstArg / secondArg).ToSpecialString());
+            }
+        }
+
+        private int numerator { get; set; }
+        private int denomerator { get; set; }
+    }
+}
+
---
+++ NeSdannoe/CSharpLab7/CSharpLab7/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace СSharpLab7
+{
+    class Program
+    {
+
+        static Fraction FractionCreation(string str)
+        {
+            int index = str.IndexOf("/");
+            if (index != -1)
+            {
+                int firstArg = int.Parse(str.Substring(0, index));
+                int secondArg = int.Parse(str.Substring(index + 1));
+                return new Fraction(firstArg, secondArg);
+            }
+            else
+            {
+                return new Fraction(int.Parse(str), 1);
+            }
+        }
+
+        static bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9' || c != '/' || c != '.')
+                    return true;
+            }
+            return false;
+        }
+        static void CheckFraction(ref string str)
+        {
+            bool isWrongSymb = CheckWrongSymbFraction(str);
+            bool isEmpty = CheckStringEmpt(ref str);
+            bool isWrongArgs = false;
+            bool isRightLength = false;
+            if (str.IndexOf("/") != -1)
+            {
+                isWrongArgs = CheckArgs(ref str, isEmpty);
+                isRightLength = (str.Length >= 3) ? false : true;
+                if (isRightLength)
+                {
+                    Console.WriteLine("Wrong line length");
+                }
+            }
+
+            while ((isEmpty != false) || (isWrongSymb != false) || (isRightLength != false) ||
+                  (isWrongArgs != false))
+            {
+                Console.WriteLine("Try again");
+                str = Console.ReadLine();
+                isWrongSymb = CheckWrongSymbFraction(str);
+                isEmpty = CheckStringEmpt(ref str);
+                if (str.IndexOf("/") != -1)
+                {
+                    isWrongArgs = CheckArgs(ref str, isEmpty);
+                    isRightLength = (str.Length >= 3) ? false : true;
+                    if (isRightLength)
+                    {
+                        Console.WriteLine("Wrong line length");
+                    }
+                }
+            }
+        }
+
+        static bool CheckArgs(ref string str, bool isEmpty)
+        {
+            bool isWrongArgs = false;
+            int index = str.IndexOf("/");
+            string firstArg = str.Substring(0, index);
+            bool isFirstArgWrong = CheckWrongSymbInt(firstArg);
+            string secondArg = str.Substring(index + 1);
+            bool isSecondArgWrong = CheckNotNullOrNegative(secondArg, isEmpty);
+            if ((isFirstArgWrong != false) || (isSecondArgWrong != false))
+            {
+                Console.WriteLine("Wrong arguments");
+                isWrongArgs = true;
+            }
+            isFirstArgWrong = CheckStringEmpt(ref firstArg);
+            isSecondArgWrong = CheckStringEmpt(ref firstArg);
+            if ((isFirstArgWrong != false) || isSecondArgWrong != false)
+            {
+                Console.WriteLine("Wrong arguments");
+                isWrongArgs = true;
+            }
+            return isWrongArgs;
+        }
+
+        static bool CheckStringEmpt(ref string str)
+        {
+            bool isEmpty = false;
+            str = str.Trim();
+            if (str.Length == 0)
+            {
+                Console.WriteLine("Empty line");
+                isEmpty = true;
+            }
+            else if (str.Length != 0)
+            {
+                isEmpty = false;
+            }
+            return isEmpty;
+        }
+
+        static bool CheckNotNullOrNegative(string str, bool isEmpty)
+        {
+            bool isNegative = false;
+            if (isEmpty == false)
+            {
+                if (str[0] == '-' || str[0] == '0')
+                {
+                    Console.WriteLine("Negative number or zero");
+                    isNegative = true;
+                }
+            }
+            return isNegative;
+        }
+
+        static void CheckNaturalInt(ref string str)
+        {
+            bool isWrongSymb = CheckWrongSymbInt(str);
+            bool isEmpty = CheckStringEmpt(ref str);
+            bool isNegativeOrNull = CheckNotNullOrNegative(str, isEmpty);
+            while ((isEmpty != false) || (isWrongSymb != false)
+                    || (isNegativeOrNull != false))
+            {
+                Console.WriteLine("Try again");
+                str = Console.ReadLine();
+                isWrongSymb = CheckWrongSymbInt(str);
+                isEmpty = CheckStringEmpt(ref str);
+                isNegativeOrNull = CheckNotNullOrNegative(str, isEmpty);
+            }
+        }
+
+        static bool CheckWrongSymbFraction(string str)
+        {
+            bool isWrongSymb = false;
+            Regex regExp = new Regex(@"[0-9]");
+            Regex regExpSlash = new Regex(@"/");
+            MatchCollection match = regExp.Matches(str);
+            MatchCollection matchSlash = regExpSlash.Matches(str);
+            if ((match.Count != str.Length) && (matchSlash.Count > 1))
+            {
+                Console.WriteLine("Incorrect input");
+                isWrongSymb = true;
+            }
+            return isWrongSymb;
+        }
+
+        static bool CheckWrongSymbInt(string str)
+        {
+            bool isWrongSymb = false;
+            Regex regExp = new Regex(@"[0-9]");
+            MatchCollection match = regExp.Matches(str);
+            if (match.Count != str.Length)
+            {
+                Console.WriteLine("Incorrect input");
+                isWrongSymb = true;
+            }
+            return isWrongSymb;
+        }
+
+        static Fraction FractionInput(ref Fraction fraction)
+        {
+            Console.WriteLine("Enter your fraction (ex. 2/3)");
+            string fractionStr = Console.ReadLine();
+            while (IsDigitsOnly(fractionStr) == false)
+            {
+                Console.WriteLine("Error. You can't use any symbols here! (except '/') \nEnter your fraction");
+                fractionStr = Console.ReadLine();
+            }
+            CheckFraction(ref fractionStr);
+            fraction = FractionCreation(fractionStr);
+            Console.WriteLine(fraction);
+            return fraction;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("What to do : \n1 - add\n2 - subtract\n3 - multiply\n4 - divide\n5 - compare\n6 - fraction into decimal \n7 - fraction into integer\n8 - exit ");
+            string operationChoiceStr = Console.ReadLine();
+            CheckNaturalInt(ref operationChoiceStr);
+            int Choice = Convert.ToInt32((operationChoiceStr));
+            Console.WriteLine("what format will the fraction be shown? \n1 - usual\n2 - special");
+            string formatChoiceStr = Console.ReadLine();
+            CheckNaturalInt(ref formatChoiceStr);
+            int formatChoice = Convert.ToInt32((formatChoiceStr));
+            Fraction fraction = new Fraction();
+            FractionInput(ref fraction);
+            Fraction fraction2 = new Fraction();
+            switch (Choice)
+            {
+                case 1:
+                    FractionInput(ref fraction2);
+                    Console.WriteLine("Sum is");
+                    Fraction.FractionSum(fraction, fraction2, formatChoice);
+                    break;
+                case 2:
+                    FractionInput(ref fraction2);
+                    Console.WriteLine("Difference is ");
+                    Fraction.FractionDiff(fraction, fraction2, formatChoice);
+                    break;
+                case 3:
+                    FractionInput(ref fraction2);
+                    Console.WriteLine("Multiplying result is ");
+                    Fraction.FractionMult(fraction, fraction2, formatChoice);
+                    break;
+                case 4:
+                    FractionInput(ref fraction2);
+                    Console.WriteLine("Dividing result is ");
+                    Fraction.FractionDiv(fraction, fraction2, formatChoice);
+                    break;
+                case 5:
+                    FractionInput(ref fraction2);
+                    if (fraction == fraction2)
+                    {
+                        Console.WriteLine("Fractions are equal");
+                    }
+                    else if (fraction > fraction2)
+                    {
+                        Console.WriteLine("First fraction is bigger");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Second fraction is bigger");
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine(((double)fraction).ToString());
+                    break;
+                case 7:
+                    Console.WriteLine(((int)fraction).ToString());
+                    break;
+                default:
+                    Console.WriteLine("Excellent choice but no");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
+
